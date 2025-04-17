53ca50b0e2256022a5f316e067ea07ef7cbe1563
Steeve Lefort <steeve@lefort-software.com>
2025-04-17 18:58:20 +0200

    update

--- lua/kanagawa/colors.lua
+++ lua/kanagawa/colors.lua
@@ -1,155 +1,151 @@
 ---@class PaletteColors
 local palette = {
-
-    -- Bg Shades
-    sumiInk0 = "#16161D",
-    sumiInk1 = "#181820",
-    sumiInk2 = "#1a1a22",
-    sumiInk3 = "#1F1F28",
-    sumiInk4 = "#2A2A37",
-    sumiInk5 = "#363646",
-    sumiInk6 = "#54546D", --fg
+    -- Bg Shades (basés sur les tons de bleu foncé de Moonlight)
+    sumiInk0 = "#161a2a", -- base0
+    sumiInk1 = "#191a2a", -- base1/bg-alt
+    sumiInk2 = "#1e2030", -- base2
+    sumiInk3 = "#212337", -- bg
+    sumiInk4 = "#222436", -- base3
+    sumiInk5 = "#2f334d", -- base4
+    sumiInk6 = "#444a73", -- base5/grey
 
     -- Popup and Floats
-    waveBlue1 = "#223249",
-    waveBlue2 = "#2D4F67",
+    waveBlue1 = "#383e5c", -- region
+    waveBlue2 = "#444a73", -- base5
 
     -- Diff and Git
-    winterGreen = "#2B3328",
-    winterYellow = "#49443C",
-    winterRed = "#43242B",
-    winterBlue = "#252535",
-    autumnGreen = "#76946A",
-    autumnRed = "#C34043",
-    autumnYellow = "#DCA561",
+    winterGreen = "#4fd6be", -- dark-teal pour winterGreen (modify)
+    winterYellow = "#ffc777", -- yellow pour winterYellow
+    winterRed = "#ff5370", -- dark-red pour winterRed
+    winterBlue = "#82aaff", -- blue pour winterBlue
+    autumnGreen = "#c3e88d", -- green pour autumnGreen
+    autumnRed = "#ff757f", -- red pour autumnRed
+    autumnYellow = "#ffc777", -- yellow pour autumnYellow
 
     -- Diag
-    samuraiRed = "#E82424",
-    roninYellow = "#FF9E3B",
-    waveAqua1 = "#6A9589",
-    dragonBlue = "#658594",
+    samuraiRed = "#ff5370", -- dark-red
+    roninYellow = "#ffc777", -- yellow
+    waveAqua1 = "#77e0c6", -- teal
+    dragonBlue = "#82aaff", -- blue
 
     -- Fg and Comments
-    oldWhite = "#C8C093",
-    fujiWhite = "#DCD7BA",
-    fujiGray = "#727169",
+    oldWhite = "#b4c2f0", -- base8/fg-alt
+    fujiWhite = "#c8d3f5", -- fg
+    fujiGray = "#7a88cf", -- indigo/comments
 
-    oniViolet = "#957FB8",
-    oniViolet2 = "#b8b4d0",
-    crystalBlue = "#7E9CD8",
-    springViolet1 = "#938AA9",
-    springViolet2 = "#9CABCA",
-    springBlue = "#7FB4CA",
-    lightBlue = "#A3D4D5", -- unused yet
-    waveAqua2 = "#7AA89F", -- improve lightness: desaturated greenish Aqua
+    -- Various purples, blues, pinks (couleurs caractéristiques de Moonlight)
+    oniViolet = "#c099ff", -- magenta
+    oniViolet2 = "#baacff", -- light-magenta
+    crystalBlue = "#82aaff", -- blue
+    springViolet1 = "#c099ff", -- magenta
+    springViolet2 = "#baacff", -- light-magenta
+    springBlue = "#50c4fa", -- light-blue
+    lightBlue = "#86e1fc", -- dark-cyan
+    waveAqua2 = "#77e0c6", -- teal
 
-    -- waveAqua2  = "#68AD99",
-    -- waveAqua4  = "#7AA880",
-    -- waveAqua5  = "#6CAF95",
-    -- waveAqua3  = "#68AD99",
+    springGreen = "#c3e88d", -- green
+    boatYellow1 = "#ffc777", -- yellow
+    boatYellow2 = "#ffc777", -- yellow
+    carpYellow = "#ffc777", -- yellow
 
-    springGreen = "#98BB6C",
-    boatYellow1 = "#938056",
-    boatYellow2 = "#C0A36E",
-    carpYellow = "#E6C384",
+    sakuraPink = "#f989d3", -- violet
+    waveRed = "#ff757f", -- red
+    peachRed = "#ff5370", -- dark-red
+    surimiOrange = "#ff995e", -- orange
+    katanaGray = "#828bb8", -- base6
 
-    sakuraPink = "#D27E99",
-    waveRed = "#E46876",
-    peachRed = "#FF5D62",
-    surimiOrange = "#FFA066",
-    katanaGray = "#717C7C",
+    -- Dragon colors (adapté avec des teintes similaires mais dans la gamme Moonlight)
+    dragonBlack0 = "#161a2a", -- base0
+    dragonBlack1 = "#191a2a", -- base1
+    dragonBlack2 = "#1e2030", -- base2
+    dragonBlack3 = "#212337", -- bg
+    dragonBlack4 = "#222436", -- base3
+    dragonBlack5 = "#2f334d", -- base4
+    dragonBlack6 = "#444a73", -- base5
 
-    dragonBlack0 = "#0d0c0c",
-    dragonBlack1 = "#12120f",
-    dragonBlack2 = "#1D1C19",
-    dragonBlack3 = "#181616",
-    dragonBlack4 = "#282727",
-    dragonBlack5 = "#393836",
-    dragonBlack6 = "#625e5a",
+    dragonWhite = "#c8d3f5", -- fg
+    dragonGreen = "#c3e88d", -- green
+    dragonGreen2 = "#77e0c6", -- teal
+    dragonPink = "#f989d3", -- violet
+    dragonOrange = "#ff995e", -- orange
+    dragonOrange2 = "#ff995e", -- orange
+    dragonGray = "#a9b8e8", -- base7
+    dragonGray2 = "#828bb8", -- base6
+    dragonGray3 = "#7a88cf", -- indigo/comments
+    dragonBlue2 = "#82aaff", -- blue
+    dragonViolet = "#c099ff", -- magenta
+    dragonRed = "#ff757f", -- red
+    dragonAqua = "#77e0c6", -- teal
+    dragonAsh = "#7a88cf", -- indigo/comments
+    dragonTeal = "#4fd6be", -- dark-teal
+    dragonYellow = "#ffc777", -- yellow
 
-    dragonWhite = "#c5c9c5",
-    dragonGreen = "#87a987",
-    dragonGreen2 = "#8a9a7b",
-    dragonPink = "#a292a3",
-    dragonOrange = "#b6927b",
-    dragonOrange2 = "#b98d7b",
-    dragonGray = "#a6a69c",
-    dragonGray2 = "#9e9b93",
-    dragonGray3 = "#7a8382",
-    dragonBlue2 = "#8ba4b0",
-    dragonViolet= "#8992a7",
-    dragonRed = "#c4746e",
-    dragonAqua = "#8ea4a2",
-    dragonAsh = "#737c73",
-    dragonTeal = "#949fb5",
-    dragonYellow = "#c4b28a",--"#a99c8b",
-    -- "#8a9aa3",
-
-    lotusInk1 = "#545464",
-    lotusInk2 = "#43436c",
-    lotusGray = "#dcd7ba",
-    lotusGray2 = "#716e61",
-    lotusGray3 = "#8a8980",
-    lotusWhite0 = "#d5cea3",
-    lotusWhite1 = "#dcd5ac",
-    lotusWhite2 = "#e5ddb0",
-    lotusWhite3 = "#f2ecbc",
-    lotusWhite4 = "#e7dba0",
-    lotusWhite5 = "#e4d794",
-    lotusViolet1 = "#a09cac",
-    lotusViolet2 = "#766b90",
-    lotusViolet3 = "#c9cbd1",
-    lotusViolet4 = "#624c83",
-    lotusBlue1 = "#c7d7e0",
-    lotusBlue2 = "#b5cbd2",
-    lotusBlue3 = "#9fb5c9",
-    lotusBlue4 = "#4d699b",
-    lotusBlue5 = "#5d57a3",
-    lotusGreen = "#6f894e",
-    lotusGreen2 = "#6e915f",
-    lotusGreen3 = "#b7d0ae",
-    lotusPink = "#b35b79",
-    lotusOrange = "#cc6d00",
-    lotusOrange2 = "#e98a00",
-    lotusYellow ="#77713f",
-    lotusYellow2 = "#836f4a",
-    lotusYellow3 = "#de9800",
-    lotusYellow4 = "#f9d791",
-    lotusRed = "#c84053",
-    lotusRed2 = "#d7474b",
-    lotusRed3 = "#e82424",
-    lotusRed4 = "#d9a594",
-    lotusAqua = "#597b75",
-    lotusAqua2 = "#5e857a",
-    lotusTeal1 = "#4e8ca2",
-    lotusTeal2 = "#6693bf",
-    lotusTeal3 = "#5a7785",
-    lotusCyan = "#d7e3d8",
+    -- Lotus colors (adapté avec la palette Moonlight)
+    lotusInk1 = "#444a73", -- base5
+    lotusInk2 = "#383e5c", -- region
+    lotusGray = "#c8d3f5", -- fg
+    lotusGray2 = "#828bb8", -- base6
+    lotusGray3 = "#a9b8e8", -- base7
+    lotusWhite0 = "#b4c2f0", -- base8/fg-alt
+    lotusWhite1 = "#c8d3f5", -- fg
+    lotusWhite2 = "#c8d3f5", -- fg
+    lotusWhite3 = "#c8d3f5", -- fg
+    lotusWhite4 = "#c8d3f5", -- fg
+    lotusWhite5 = "#c8d3f5", -- fg
+    lotusViolet1 = "#c099ff", -- magenta
+    lotusViolet2 = "#baacff", -- light-magenta
+    lotusViolet3 = "#f3c1ff", -- pink
+    lotusViolet4 = "#c099ff", -- magenta
+    lotusBlue1 = "#82aaff", -- blue
+    lotusBlue2 = "#50c4fa", -- light-blue
+    lotusBlue3 = "#86e1fc", -- dark-cyan
+    lotusBlue4 = "#4976eb", -- dark-blue
+    lotusBlue5 = "#82aaff", -- blue
+    lotusGreen = "#c3e88d", -- green
+    lotusGreen2 = "#77e0c6", -- teal
+    lotusGreen3 = "#7af8ca", -- light-teal
+    lotusPink = "#f989d3", -- violet
+    lotusOrange = "#ff995e", -- orange
+    lotusOrange2 = "#ff995e", -- orange
+    lotusYellow = "#ffc777", -- yellow
+    lotusYellow2 = "#ffc777", -- yellow
+    lotusYellow3 = "#ffc777", -- yellow
+    lotusYellow4 = "#ffc777", -- yellow
+    lotusRed = "#ff757f", -- red
+    lotusRed2 = "#ff5370", -- dark-red
+    lotusRed3 = "#ff5370", -- dark-red
+    lotusRed4 = "#ff98a4", -- light-red
+    lotusAqua = "#77e0c6", -- teal
+    lotusAqua2 = "#4fd6be", -- dark-teal
+    lotusTeal1 = "#4fd6be", -- dark-teal
+    lotusTeal2 = "#77e0c6", -- teal
+    lotusTeal3 = "#86e1fc", -- dark-cyan
+    lotusCyan = "#b4f9f8", -- cyan
 }
 
 local M = {}
 --- Generate colors table:
 --- * opts:
 ---   - colors: Table of personalized colors and/or overrides of existing ones.
----     Defaults to KanagawaConfig.colors.
----   - theme: Use selected theme. Defaults to KanagawaConfig.theme
+---     Defaults to MoonlightConfig.colors.
+---   - theme: Use selected theme. Defaults to MoonlightConfig.theme
 ---     according to the value of 'background' option.
 ---@param opts? { colors?: table, theme?: string }
 ---@return { theme: ThemeColors, palette: PaletteColors}
 function M.setup(opts)
     opts = opts or {}
-    local override_colors = opts.colors or require("kanagawa").config.colors
-    local theme = opts.theme or require("kanagawa")._CURRENT_THEME -- WARN: this fails if called before kanagawa.load()
+    local override_colors = opts.colors or require("moonlight").config.colors
+    local theme = opts.theme or require("moonlight")._CURRENT_THEME -- WARN: this fails if called before moonlight.load()
 
     if not theme then
-        error("kanagawa.colors.setup(): Unable to infer `theme`. Either specify a theme or call this function after ':colorscheme kanagawa'")
+        error("moonlight.colors.setup(): Unable to infer `theme`. Either specify a theme or call this function after ':colorscheme moonlight'")
     end
 
     -- Add to and/or override palette_colors
     local updated_palette_colors = vim.tbl_extend("force", palette, override_colors.palette or {})
 
     -- Generate the theme according to the updated palette colors
-    local theme_colors = require("kanagawa.themes")[theme](updated_palette_colors)
+    local theme_colors = require("moonlight.themes")[theme](updated_palette_colors)
 
     -- Add to and/or override theme_colors
     local theme_overrides = vim.tbl_deep_extend("force", override_colors.theme["all"] or {}, override_colors.theme[theme] or {} )
